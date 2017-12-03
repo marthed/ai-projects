f8e4ad0ff0d3f910ebc48acfb8b8f5c59bbdda2d
marthed <martinhedlund@live.com>
2017-12-03 23:09:59 +0100

    xperteleven scraper start

--- crawler.js
+++ crawler.js
@@ -14,6 +14,16 @@ request(pageToVisit, function(error, response, body) {
    if(response.statusCode === 200) {
      // Parse the document body
      var $ = cheerio.load(body);
-     console.log("Page title:  " + $('title').text());
+     //console.log("Page title:  " + $('title').text());
+     $('tr.ItemStyleGrey').each(function( idx ) {
+       var text = $(this).text().trim();
+       console.log('text', text);
+     })
    }
 });
+
+
+function searchForWord($, word) {
+  var bodyText = $('html > body').text().toLowerCase();
+  return(bodyText.indexOf(word.toLowerCase()) !== -1);
+}
