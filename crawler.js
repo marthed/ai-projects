0a31fa843cc13dc4d88424f4a8f355b8a6eb260f
marthed <martinhedlund@live.com>
2017-12-03 22:41:51 +0100

    crawler

---
+++ crawler.js
@@ -0,0 +1,19 @@
+var request = require('request');
+var cheerio = require('cheerio');
+var URL = require('url-parse');
+
+
+var pageToVisit = "http://www.xperteleven.com/standings.aspx?Lid=333091&Sel=T&Lnr=1&dh=2";
+console.log("Visiting page " + pageToVisit);
+request(pageToVisit, function(error, response, body) {
+   if(error) {
+     console.log("Error: " + error);
+   }
+   // Check status code (200 is HTTP OK)
+   console.log("Status code: " + response.statusCode);
+   if(response.statusCode === 200) {
+     // Parse the document body
+     var $ = cheerio.load(body);
+     console.log("Page title:  " + $('title').text());
+   }
+});
