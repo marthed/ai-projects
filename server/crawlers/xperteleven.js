20d4ab3f80264c6622633d74616935acb44e53e5
marthed <martinhedlund@live.com>
2017-12-05 22:24:11 +0100

    better crawling

--- server/crawlers/xperteleven.js
+++ server/crawlers/xperteleven.js
@@ -8,31 +8,30 @@ async function getStandings() {
   var pageToVisit = "http://www.xperteleven.com/standings.aspx?Lid=333091&Sel=T&Lnr=1&dh=2";
   console.log('get standings');
 
-  return Promise.apply(request(pageToVisit, function(error, response, body) {
-    if(error) {
-      console.log("xperteleven standings crawl error: " + error);
-      return error;
-    }
-
-    console.log("xperteleven standings crawl statusCode: " + response.statusCode);
-    if(response.statusCode === 200) {
-
-      // Parse the document body
-      var $ = cheerio.load(body);
+  return axios.request(
+    {
+      url: pageToVisit,
+      method: 'get'
+    })
+    .then((res) => {
+      var $ = cheerio.load(res.data);
       var dataList = [];
-      //console.log("Page title:  " + $('title').text());
-      $('tr.ItemStyleGrey').each(function( idx ) {
-        var text = $(this).text().trim();
+      console.log($('tr', '#ctl00_cphMain_dgStandings'));
+      $('tr', '#ctl00_cphMain_dgStandings').each(function( idx ) {
+
+        console.log('text', $(this).text());
+        var text = $(this).text().replace(/\s/g,'');
         if(text !== ''){
           dataList.push(text);
         }
       });
       console.log('dataList', dataList);
-      return dataList;
-    }
-    return 'sdjfsdf'
- }));
-}
+      return Promise.resolve(dataList);
+    })
+    .catch((error) => {
+      return Promise.reject(error);
+    });
+  }
 
 module.exports = {
   getStandings
