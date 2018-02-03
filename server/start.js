b7e293a2e7bbd8e385687c99870e787c6c91db6c
marthed <martinhedlund@live.com>
2018-02-03 23:44:58 +0100

    log environment

--- server/start.js
+++ server/start.js
@@ -3,4 +3,5 @@ app.set('port', process.env.PORT || 7777);
 
 const server = app.listen(app.get('port'), () => {
   console.log(`Express running → PORT ${server.address().port}`);
+  console.log('Environment: ', process.env);
 });
